0d8066b389ed29bbd2bd88320c013cb25a9de8bb
AhnDaeHoon <45304096+dh0730@users.noreply.github.com>
2025-03-31 01:55:26 +0900

    Update kakao.js

--- api/kakao.js
+++ api/kakao.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   
   try {
     // GAS 웹앱 URL (배포된 Apps Script 웹앱 URL)
-    const gasUrl = 'https://script.google.com/macros/s/AKfycbyuCnROudB3xHlHPALfopIPbOcf_paUsMfrjMjZbh63bp99UlZYfaVgEEeMQUJKjO9b/exec';
+    const gasUrl = 'https://script.google.com/macros/s/AKfycbxRbQSYRzDiyecKuduz1kNCEEhdk3qCnUX5iqWx6fcQ3X6c9dCilxiAKJOUbYTeY6tB/exec';
 
     // GAS에 보낼 데이터
     const gasResponse = await axios.post(gasUrl, {
