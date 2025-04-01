f1dc98cd7da7c66b66fdfa19e2bdda3087483c8a
AhnDaeHoon <45304096+dh0730@users.noreply.github.com>
2025-04-01 18:04:11 +0900

    Update kakao.js

--- api/kakao.js
+++ api/kakao.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   const body = req.body;
-  const gasUrl = 'https://script.google.com/macros/s/AKfycbyGwbTZbWeC3Z870Guk3XpDxIaj86vcb582rPPnFLRwgdlWtxxKzOGCj_2EFX8-rDDU/exec';
+  const gasUrl = 'https://script.google.com/macros/s/AKfycbwYeHR9UD4io_Qidr2tnWuYBL_yEF4LmgC047RsRY7m0KJBs_vfkcxdgVljFmsamKGt/exec';
 
   console.log("카카오 요청 수신:", JSON.stringify(body));
 
