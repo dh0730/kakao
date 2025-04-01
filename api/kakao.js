4d2016e8c82214301361e6859073b60cbdcf751a
AhnDaeHoon <45304096+dh0730@users.noreply.github.com>
2025-04-01 17:17:14 +0900

    Update kakao.js

--- api/kakao.js
+++ api/kakao.js
@@ -24,6 +24,76 @@ export default async function handler(req, res) {
     }
   };
 
+
+  if (body.action.params.type === "type_select2") {
+  const utterance = body.userRequest.utterance;
+  const blockName = body.flow.lastBlock.name;
+
+  if (blockName === "견적서5") {
+    response = {
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            basicCard: {
+              title: `입력된 출발지 : ${utterance}`,
+              description: "도착지를 입력해볼까요?",
+              buttons: [
+                {
+                  action: "block",
+                  label: "도착지 입력하러가기!",
+                  blockId: "67eaeab3a6c9712a60fbb7c4"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    };
+  } else if (blockName === "견적서6") {
+    response = {
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            basicCard: {
+              title: `입력된 도착지 : ${utterance}`,
+              description: "입력된 내용을 확인해볼까요?",
+              buttons: [
+                {
+                  action: "block",
+                  label: "네!",
+                  blockId: "67ead98ce740af7a5e26563c"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    };
+  }
+} else if (body.action.params.type === "type_last") {
+  response = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          basicCard: {
+            title:
+              `📝 입력해주신 정보, 이렇게 확인했어요! \n` +
+              `🚚 출발지: ㅇㅇ\n\n🏡 도착지: ㅂㅂ\n📅 이사 날짜: {{날짜}} {{시간}}\n` +
+              `📦 평수: {{평수}}, 짐량: {{짐량}}, 엘리베이터: {{엘리베이터}}\n\n` +
+              `📌 확인 완료! \n` +
+              `이제 가장 중요한 단계예요 🔍\n✅ 고객님 조건에 딱 맞는 후기 좋은 이사업체 3곳에 실시간 견적 요청 중입니다 😊\n\n` +
+              `⏳ 1분만 기다려주세요...`
+          }
+        }
+      ]
+    }
+  };
+}
+  
+
   // 💡 먼저 카카오 챗봇에 응답 보내기 (GAS 처리 기다리지 않음)
   res.status(200).json(response);
 
