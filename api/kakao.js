965ade2c8b78b7cd7899c9e4f1d267086e76ce69
AhnDaeHoon <45304096+dh0730@users.noreply.github.com>
2025-04-07 21:32:14 +0900

    Update kakao.js

--- api/kakao.js
+++ api/kakao.js
@@ -8,7 +8,7 @@ import axios from 'axios';
  
    // 요청 body 받기
    const body = req.body;
-   var gasUrl = 'https://script.google.com/macros/s/AKfycbzThLaGNWQMZy0nkgx-2DgIv3-K0pcrtClXLQMJRvPE8NQ8J8oyQWkf8FY7DtziTQUg/exec';
+   var gasUrl = 'https://script.google.com/macros/s/AKfycbyFwHoUpeyO_ZU0xi2skSD4akz9gQmpObmuuDCXMYQkFDAA83my08ozVtJoQlWbJ0iu/exec';
    var gasResponse = '';
    // 로그 찍기 (Vercel dashboard > Logs 에서 확인 가능)
    console.log("카카오 요청 수신:", JSON.stringify(body));
