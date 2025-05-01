f03aaf2e711d278f66f8bc820a2f7807a79fb60c
AhnDaeHoon <45304096+dh0730@users.noreply.github.com>
2025-05-02 02:40:49 +0900

    Update kakao.js

--- api/kakao.js
+++ api/kakao.js
@@ -28,6 +28,7 @@ import axios from 'axios';
        } catch (error) {
          console.error("GAS 호출 오류:", error);
        } finally{
+         console.log(gasResponse);
          return res.status(200).json({"dddd" : "success"});
        }
    }
